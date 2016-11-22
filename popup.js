5ababda252fad7449438583189319da715dea361
Alex Bennett <kestralttr@Alex-Bennetts-MacBook-Pro.local>
2016-11-22 11:18:59 -0800

    chains storage API sets and gets to respective button and fields in popup.html

--- popup.js
+++ popup.js
@@ -1,20 +1,36 @@
 
 
 document.body.onload = function() {
-  chrome.storage.sync.clear(alert("cleared"));
-  chrome.storage.sync.set(
-    {"phone-number": 123456789},
-    alert("save successful")
-  );
 
   chrome.storage.sync.get(
     "phone-number",
     function(item) {
-      if (!chrome.runtime.error) {
-        console.log(item);
+      if (!chrome.runtime.error && item["phone-number"] !== undefined) {
         document.getElementById("phone-number").value = item["phone-number"];
       }
     }
   );
 
+  chrome.storage.sync.get(
+    "alert-message",
+    function(item) {
+      if (!chrome.runtime.error && item["alert-message"] !== undefined) {
+        document.getElementById("alert-message").value = item["alert-message"];
+      }
+    }
+  );
+
+};
+
+document.getElementById("set-data").onclick = function() {
+  let pn = document.getElementById("phone-number").value;
+  let am = document.getElementById("alert-message").value;
+  chrome.storage.sync.clear();
+  chrome.storage.sync.set(
+    {"phone-number": pn}
+  );
+  chrome.storage.sync.set(
+    {"alert-message": am}
+  );
+  window.close();
 };
