5e09445f4acc8ffc522210125e71ca71d3b6cc41
Alex Bennett <kestralttr@Alex-Bennetts-MacBook-Pro.local>
2016-11-22 09:36:40 -0800

    updates API call to text my number

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.commands.onCommand.addListener(function(command) {
       type: "POST",
       url: "http://textbelt.com/text",
       data: {
-        number: 2096145459,
+        number: 6308053330,
         message:"Guess who got his API working?  Mwahahaha."
       },
       success: alert("AJAX request successful!")
