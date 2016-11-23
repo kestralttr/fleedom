44ccf328821ba0eb991dd85f90fb8eb0d247af1a
Alex Bennett <kestralttr@Alex-Bennetts-MacBook-Pro.local>
2016-11-23 13:42:02 -0800

    adds badge display and removal upon firing text hotkey

--- background.js
+++ background.js
@@ -5,15 +5,27 @@ chrome.commands.onCommand.addListener(function(command) {
       "phone-number",
       function(item) {
         if (!chrome.runtime.error && item["phone-number"] !== undefined) {
-          $.ajax({
-            type: "POST",
-            url: "http://textbelt.com/text",
-            data: {
-              number: item["phone-number"],
-              message:"Thank you for using Fleedom!"
-            },
-            success: alert("AJAX request successful!")
-          });
+          // $.ajax({
+          //   type: "POST",
+          //   url: "http://textbelt.com/text",
+          //   data: {
+          //     number: item["phone-number"],
+          //     message:"Thank you for using Fleedom!"
+          //   },
+          //   success: alert("AJAX request successful!")
+          // });
+          // alert("stand-in request");
+
+
+          setTimeout(
+            function() {chrome.browserAction.setBadgeText({text: "T"});
+              chrome.browserAction.setBadgeBackgroundColor({color: "gray"})},
+            50
+          );
+          setTimeout(
+            function() {chrome.browserAction.setBadgeText({text: ""});},
+            750
+          );
         }
       }
     );
